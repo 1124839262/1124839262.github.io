061afbf71be41bf82ca973f1d5388e9c78cb7cc4
1124839262@qq.com <qq1277495912>
2026-03-20 10:59:24 +0800

    Update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1773907987616";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1773972928070";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
